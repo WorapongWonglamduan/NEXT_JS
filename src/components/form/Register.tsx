f2891b063ac8a7c0015e0d90097ef81e355b4dea
WorapongWonglamduan <the_oo_ooo@hotmail.com>
2024-06-24 23:18:11 +0700

    update

--- src/components/form/Register.tsx
+++ src/components/form/Register.tsx
@@ -3,6 +3,7 @@ import Inputs from "../inputs/Inputs";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import zxcvbn from "zxcvbn";
 const FormSchema = z
   .object({
     first_name: z
@@ -30,6 +31,8 @@ interface IRegisterProps {}
 //   exampleRequired: string;
 // };
 const Register: React.FunctionComponent<IRegisterProps> = (props) => {
+  const [passwordScore, setPasswordScore] = React.useState<number>(0);
+
   const {
     register,
     handleSubmit,
@@ -38,59 +41,87 @@ const Register: React.FunctionComponent<IRegisterProps> = (props) => {
   } = useForm<FormSchemaType>({ resolver: zodResolver(FormSchema) });
   const onSubmit: SubmitHandler<FormSchemaType> = (data) => console.log(data);
 
+  const validatePassword = () => {
+    let password = watch().password;
+
+    return zxcvbn(password ? password : "").score;
+  };
+  React.useEffect(() => {
+    setPasswordScore(validatePassword());
+  }, [watch().password]);
+
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h1>Register form</h1>
-        <Inputs
-          name="first_name"
-          label="First_Name"
-          type="text"
-          placeholder="First Name"
-          register={register}
-          error={errors?.first_name?.message}
-          disable={isSubmitting}
-        />
+      <h1>Register form</h1>
+      <div className="w-full px-12 py-4">
+        <form className="my-8 text-sm" onSubmit={handleSubmit(onSubmit)}>
+          <Inputs
+            name="first_name"
+            label="First_Name"
+            type="text"
+            placeholder="First Name"
+            register={register}
+            error={errors?.first_name?.message}
+            disable={isSubmitting}
+          />
 
-        <Inputs
-          name="last_name"
-          label="Last_Name"
-          type="text"
-          placeholder="Last Name"
-          register={register}
-          error={errors?.last_name?.message}
-          disable={isSubmitting}
-        />
-        <Inputs
-          name="email"
-          label="Email"
-          type="text"
-          placeholder="Email"
-          register={register}
-          error={errors?.email?.message}
-          disable={isSubmitting}
-        />
-        <Inputs
-          name="password"
-          label="Password"
-          type="text"
-          placeholder="Password"
-          register={register}
-          error={errors?.password?.message}
-          disable={isSubmitting}
-        />
-        <Inputs
-          name="confirm_password"
-          label="Confirm Password"
-          type="text"
-          placeholder="Confirm Password"
-          register={register}
-          error={errors?.confirm_password?.message}
-          disable={isSubmitting}
-        />
+          <Inputs
+            name="last_name"
+            label="Last_Name"
+            type="text"
+            placeholder="Last Name"
+            register={register}
+            error={errors?.last_name?.message}
+            disable={isSubmitting}
+          />
+          <Inputs
+            name="email"
+            label="Email"
+            type="text"
+            placeholder="Email"
+            register={register}
+            error={errors?.email?.message}
+            disable={isSubmitting}
+          />
+          <Inputs
+            name="password"
+            label="Password"
+            type="password"
+            placeholder="Password"
+            register={register}
+            error={errors?.password?.message}
+            disable={isSubmitting}
+          />
+          {watch().password?.length > 0 && (
+            <div className="flex mt-2">
+              {Array.from(Array(5).keys()).map((item, i) => (
+                <span className="w-1/5 px-1" key={i}>
+                  <div
+                    className={`h-2 ${
+                      passwordScore <= 2
+                        ? "bg-red-500"
+                        : passwordScore < 4
+                        ? "bg-yellow-500"
+                        : "bg-green-500"
+                    }`}
+                  ></div>
+                </span>
+              ))}
+            </div>
+          )}
+          <Inputs
+            name="confirm_password"
+            label="Confirm Password"
+            type="password"
+            placeholder="Confirm Password"
+            register={register}
+            error={errors?.confirm_password?.message}
+            disable={isSubmitting}
+          />
 
-        <input type="submit" />
-      </form>
+          <input type="submit" />
+        </form>
+      </div>
     </>
   );
 };
--- src/components/inputs/Inputs.tsx
+++ src/components/inputs/Inputs.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-
+import { CiCircleAlert } from "react-icons/ci";
+import { ImEye, ImEyeBlocked } from "react-icons/im";
 interface IInputsProps {
   name: string;
   label: string;
@@ -11,18 +12,38 @@ interface IInputsProps {
 }
 
 const Inputs: React.FunctionComponent<IInputsProps> = (props) => {
+  const [showpassword, setShowpassword] = React.useState(false);
   const { name, label, type, placeholder, register, error, disable } = props;
   return (
     <>
-      <h1>Inputs</h1>
-
-      <input
-        name={name}
-        type={type}
-        placeholder={placeholder}
-        {...register(name)}
-      />
-      {error}
+      <div className="nt-3 w-[100%]">
+        <label htmlFor={name}>{label}</label>
+        <div className="relative">
+          <input
+            className="w-full border py-2 pr-5 pl-8"
+            name={name}
+            type={showpassword ? "text" : type}
+            placeholder={placeholder}
+            {...register(name)}
+            style={{ borderColor: `${error ? "#ED4337" : ""}` }}
+          />
+          {(name === "password" || name === "confirm_password") && (
+            <div
+              className="absolute top-3 right-3 cursor-pointer"
+              style={{ right: `${error ? "2rem" : ""}` }}
+              onClick={() => setShowpassword((p) => !p)}
+            >
+              {showpassword ? <ImEye /> : <ImEyeBlocked />}
+            </div>
+          )}
+          {error && (
+            <div className="fill-red-500 absolute right-2 top-2 text-xl">
+              <CiCircleAlert fill="red" />
+            </div>
+          )}
+          {error && <p className="text-sm text-red-500">{error}</p>}
+        </div>
+      </div>
     </>
   );
 };
